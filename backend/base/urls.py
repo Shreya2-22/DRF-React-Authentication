d3f7922b4d569a562e19da1fd56f3e02e75a523e
Shreya2-22 <kshreya22@tbc.edu.np>
2025-06-19 14:46:51 +0545

    build refresh token cookies

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -1,12 +1,8 @@
 from django.urls import path
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
-from .views import get_notes, CustomTokenObtainPairView
+from .views import get_notes, CustomTokenObtainPairView, CustomRefreshTokenView
 
 urlpatterns = [
     path('token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/refresh/', CustomRefreshTokenView.as_view(), name='token_refresh'),
     path('notes/', get_notes, name='get_notes'),
 ]
--- backend/base/views.py
+++ backend/base/views.py
@@ -38,6 +38,30 @@ class CustomTokenObtainPairView(TokenObtainPairView):
             return res
         except:
             return response({'success': False})
+        
+
+class CustomRefreshTokenView(TokenRefreshView):
+    def post(self, request, *args, **kwargs):
+        try:
+            refresh_token = request.COOKIES.get('refresh_token')
+            request.data['refresh'] = refresh_token
+            response = super().post(request, *args, **kwargs)
+            tokens = response.data
+            access_token = tokens['access']
+            res = Response()
+            res.data = {'refreshed':True}
+            res.set_cookie(
+                key = 'access_token',
+                value = access_token,
+                httponly = True,
+                secure = True,
+                samesite = 'None',
+                path='/'
+            )
+            return res
+        except:
+            return Response({'refreshed':False})
+
 
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
