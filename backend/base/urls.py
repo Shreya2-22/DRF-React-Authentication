8c3303dff05ca8c786c410c2145d79bd5127a75a
Shreya2-22 <kshreya22@tbc.edu.np>
2025-06-19 14:34:39 +0545

    build access token cookies

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -3,10 +3,10 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
-from .views import get_notes
+from .views import get_notes, CustomTokenObtainPairView
 
 urlpatterns = [
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('notes/', get_notes, name='get_notes'),
 ]
--- backend/base/views.py
+++ backend/base/views.py
@@ -5,6 +5,39 @@ from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.response import Response
 from .models import Note
 from .serializers import NoteSerializer
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+class CustomTokenObtainPairView(TokenObtainPairView):
+    def post(self, request, *args, **kwargs):
+        try:
+            response = super().post(request, *args, **kwargs)
+            tokens = response.data
+            access_token = tokens['access']
+            refresh_token = tokens['refresh']
+            res = Response()
+            res.data = {'success':True}
+            res.set_cookie(
+                key = "access_token",
+                value = access_token,
+                httponly = True,
+                secure = True,
+                samesite = 'None',
+                path ='/'
+            )
+            res.set_cookie(
+                key = "refresh_token",
+                value = refresh_token,
+                httponly = True,
+                secure = True,
+                samesite = 'None',
+                path ='/'
+            )
+            return res
+        except:
+            return response({'success': False})
 
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
