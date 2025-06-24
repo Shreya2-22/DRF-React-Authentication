beb4ebac1eb587531ffa51f0e1278b33664d4148
Shreya2-22 <kshreya22@tbc.edu.np>
2025-06-24 12:51:37 +0545

    build user menu route

--- frontend/src/endpoints/api.js
+++ frontend/src/endpoints/api.js
@@ -2,6 +2,9 @@ import axios from 'axios';
 
 const BASE_URL = 'http://127.0.0.1:8000/api/';
 const LOGIN_URL = `${BASE_URL}token/`;
+const REFRESH_URL = `${BASE_URL}token/refresh/`;
+const NOTES_URL = `${BASE_URL}notes/`;
+const LOGOUT_URL = `${BASE_URL}logout/`;
 
 export const login = async (username, password) => {
     const response = await axios.post(LOGIN_URL, {
@@ -11,3 +14,41 @@ export const login = async (username, password) => {
     {withCredentials: true});
     return response.data.success
 };
+
+export const refresh_token = async () => {
+    try {
+        await axios.post(REFRESH_URL, {}, {withCredentials: true});
+        return true;
+    } catch (error) {
+        return false;
+    }
+}
+
+export const get_notes = async() => {
+    try {
+        const response = await axios.get(NOTES_URL, {withCredentials: true});
+        return response.data;
+    } catch (error) {
+        return call_refresh(error, axios.get(NOTES_URL, {withCredentials: true}));
+    }
+}
+
+const call_refresh = async (error, func) => {
+    if (error.response && error.response.status === 401){
+        const tokenRefreshed = await refresh_token();
+        if (tokenRefreshed) {
+            const retryResponse = await func();
+            return retryResponse.data;
+        }
+    }
+    return false;
+}
+
+export const logout = async () => {
+    try {
+        await axios.post(LOGOUT_URL, {}, {withCredentials: true});
+        return true;
+    } catch (error) {
+        return false;
+    }
+}
--- frontend/src/routes/menu.js
+++ frontend/src/routes/menu.js
@@ -1,6 +1,40 @@
+import { VStack, Heading, Text, Button } from "@chakra-ui/react"
+import { useEffect, useState } from "react"
+import {get_notes, logout} from '../endpoints/api'
+import {useNavigate} from 'react-router-dom';
 
-const menu = () => {
-  return <h1>Menu</h1>
+const Menu = () => {
+    const [notes, setNotes] = useState([])
+    const nav = useNavigate();
+    useEffect(()=>{
+        const fetchNotes = async () => {
+            const notes = await get_notes()
+            setNotes(notes)
+        }
+        fetchNotes();
+    },[])
+
+    const handleLogout = async() => {
+        const success = await logout();
+        if(success){
+            nav('/login')
+        }
+    }
+
+  return (
+    <VStack>
+        <Heading>Welcome back User</Heading>
+            <VStack>
+                {notes.map((note)=>{
+                    return <Text>{note.description}</Text>
+                })}
+            </VStack>
+            <Button onClick={handleLogout} 
+                bg="red.500" 
+                color="white"
+                _hover={{ bg: "red.600" }}>Logout</Button>
+    </VStack>
+  )
 }
 
-export default menu
+export default Menu
